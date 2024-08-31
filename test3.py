fd055e03d95b3ed9cd8807e4426983d8a7a237e7
LAMPV <lampvhy@gmail.com>
2024-08-31 14:49:28 +0700

    update example

--- test3.py
+++ test3.py
@@ -16,6 +16,7 @@ Plotting of orderflow chart
 '''
 
 stock = sys.argv[1]
+type = sys.argv[2]
 
 if __name__ == '__main__':
 	inst = 'btcusdt'
@@ -23,22 +24,24 @@ if __name__ == '__main__':
 	token = inst.upper()
 	interval = '1D'
 
-	# ohlcv = pd.read_csv('examples/data/sample_data_2.csv', index_col=0)
-	ohlcv = pd.read_csv(f"./data/price_and_delta/{stock.upper()}-pd.csv", index_col=0)
-	ohlcv.index = pd.to_datetime(ohlcv.index)
+	if type == 'day':
+		ohlcv = pd.read_csv(f"./../datalake-vni/p_d/{stock}-pd.csv", index_col=0)
+		profile = pd.read_csv(f"./../datalake-vni/stack_orderflow/{stock}-orderflow.csv")
+	elif type == 'hour':
+		ohlcv = pd.read_csv(f"./../datalake-vni/p_d_hour/{stock.upper()}-pd-hour.csv", index_col=0)
+		profile = pd.read_csv(f"./../datalake-vni/stack_orderflow_hour/{stock.upper()}-orderflow.csv")
 
-	profile = pd.read_csv(f"./data/stack_orderflow/{stock.upper()}-orderflow.csv")
-	# profile = pd.read_csv('examples/data/orderflow_data.csv')
 	profile['t'] = pd.to_datetime(profile['t'])
+	ohlcv.index = pd.to_datetime(ohlcv.index)
 
 	mp_slices=[]
 	dts = list(ohlcv.index)
 	for dt in dts:
-	    ohlcv_data = ohlcv.loc[dt].to_dict()
-	    orderflow_data = profile[profile['t'] == dt][['p', 'q', 'd', 'b', 'a']].to_numpy()
-	    print(ohlcv_data)
-	    print(orderflow_data)
-	    mp_slices.append(MarketProfileSlice(inst, dt, ohlcv_data, orderflow_data))
+		ohlcv_data = ohlcv.loc[dt].to_dict()
+		orderflow_data = profile[profile['t'] == dt][['p', 'q', 'd', 'b', 'a']].to_numpy()
+		print(ohlcv_data)
+		print(orderflow_data)
+		mp_slices.append(MarketProfileSlice(inst, dt, ohlcv_data, orderflow_data))
 
 	plotter = OrderflowPlotter(token, interval, increment, ohlcv, mp_slices)
 	plotter.orderflow_plot()
